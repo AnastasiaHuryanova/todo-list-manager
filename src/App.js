6e197746f9fc0f8ebd08f223911b407bb5d1768c
Nastia <anastasia39@hotmail.it>
2022-10-02 23:24:14 +0200

    Setup task

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import {todos} from "./assets/todos";
 
 function App() {
-  return (
-    <div className="App">
+    // todos (che trovi nel file todos.js) é un array che contiene varie attivitá da svolgere
+    // Ognuna delle attivitá a delle properietá ovvero:
+    // completed che é un boolean
+    // id che é un numero intero
+    // todo che é una stringa
+    // userId che é un numero intero
+    console.log(todos)
+    //L'obiettivo é quello di mostrare nella pagina la lista di attivitá da svolgere
 
-    </div>
-  );
+    // Consiglio: puoi creare dei piccoli componenti come ad esempio:
+    const todo0 = <div><p>{todos[0].todo}</p></div>
+    const todo1 = <div><p>{todos[1].todo}</p></div>
+
+    // E poi puoi inserirli nel return:
+    return (
+        <div className="App">
+            {todo0}
+            {todo1}
+        </div>
+    );
+
+    // Potresti completare questa task facendo copia incolla per ogni todo nella lista, ma l'obiettivo é quello di farlo utilizzando un ciclo (quello che preferisci, vedi: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration) oppure in alternativa una funzione degli oggetti Array (vedi: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array)
 }
 
 export default App;
