1ae74eb7240f16e7d1cac8389ffd6b67fd114174
Nastia <anastasia39@hotmail.it>
2022-10-03 15:51:30 +0200

    Done!

--- src/App.js
+++ src/App.js
@@ -12,16 +12,24 @@ function App() {
     //L'obiettivo é quello di mostrare nella pagina la lista di attivitá da svolgere
 
     // Consiglio: puoi creare dei piccoli componenti come ad esempio:
-    const todo0 = <div><p>{todos[0].todo}</p></div>
-    const todo1 = <div><p>{todos[1].todo}</p></div>
+
 
     // E poi puoi inserirli nel return:
-    return (
-        <div className="App">
-            {todo0}
-            {todo1}
-        </div>
-    );
+
+
+        const toBeDone = todos.filter(activity => activity.completed === false);
+                return (
+                    <div className="App">
+                        {toBeDone.map(activity => {
+                            return (
+                                <li key={activity.id}>
+                                    {activity.todo}
+                                </li>
+                            )
+                        })}
+                    </div>
+                );
+
 
     // Potresti completare questa task facendo copia incolla per ogni todo nella lista, ma l'obiettivo é quello di farlo utilizzando un ciclo (quello che preferisci, vedi: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration) oppure in alternativa una funzione degli oggetti Array (vedi: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array)
 }
